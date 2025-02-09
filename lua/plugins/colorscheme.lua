7d7e8d72cbc67406fa8231e81473020a4f9e8977
DoubleOMangoo <mhafizidlan@gmail.com>
2025-02-09 16:02:17 +0800

    Changed colorscheme to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,8 +2,9 @@ return{
 	"folke/tokyonight.nvim",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme tokyonight]])
+	config = function(_,opts)
+		local tokyonight = require("tokyonight")
+		tokyonight.setup(opts)
+		tokyonight.load()
 	end,
 }
